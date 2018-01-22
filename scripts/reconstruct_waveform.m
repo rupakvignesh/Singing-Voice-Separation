176a434cf392d7141cb0b352b704ff4a435fa724
Rupak Vignesh Swaminathan <rupakvignesh@gmail.com>
2018-01-21 19:34:05 -0500

    now returns estimated instrument as well

--- scripts/reconstruct_waveform.m
+++ scripts/reconstruct_waveform.m
@@ -1,18 +1,26 @@
-function [x,fs] = reconstruct_waveform(est_mag_spec, orig_sig, win_size, hop_size, fs)
+function [est_vocal, est_inst, fs] = reconstruct_waveform(est_vocal_spec, est_inst_spec, orig_sig, win_size, hop_size, fft_size, fs)
 
-num_blocks = size(est_mag_spec,2);
+num_blocks = size(est_vocal_spec,2);
 hann_win = hann(win_size);
-orig_X = spectrogram(orig_sig, hann_win, win_size-hop_size, win_size, fs);
+orig_X = spectrogram(orig_sig, hann_win, win_size-hop_size, fft_size, fs);
 orig_X_phase = angle(orig_X);
 
-rec_spec = bsxfun(@times, est_mag_spec, exp(1i .* orig_X_phase));
+rec_vocal_spec = bsxfun(@times, est_vocal_spec, exp(1i .* orig_X_phase));
+rec_inst_spec = bsxfun(@times, est_inst_spec, exp(1i .* orig_X_phase));
 
-len_x = (num_blocks-1)*hop_size + win_size;
-x = zeros(len_x, 1);
+len_x = (num_blocks-1)*hop_size + fft_size;
+est_vocals = zeros(len_x, 1);
 for i=1:num_blocks
-    temp_sig = ifft([rec_spec(:,i); conj(flipud(rec_spec(2:end-1,i)))]);
+    temp_sig = ifft([rec_vocal_spec(:,i); conj(flipud(rec_vocal_spec(2:end-1,i)))]);
     temp_sig = real(temp_sig);
-    x((i-1)*hop_size+1:(i-1)*hop_size+win_size) = x((i-1)*hop_size+1:(i-1)*hop_size+win_size) + temp_sig;
+    est_vocals((i-1)*hop_size+1:(i-1)*hop_size+fft_size) = est_vocals((i-1)*hop_size+1:(i-1)*hop_size+fft_size) + hann(fft_size).*temp_sig;
+end
+
+est_inst = zeros(len_x, 1);
+for i=1:num_blocks
+    temp_sig = ifft([rec_vocal_spec(:,i); conj(flipud(rec_vocal_spec(2:end-1,i)))]);
+    temp_sig = real(temp_sig);
+    est_inst((i-1)*hop_size+1:(i-1)*hop_size+fft_size) = est_inst((i-1)*hop_size+1:(i-1)*hop_size+fft_size) + hann(fft_size).*temp_sig;
 end
 
 end
