7e01371e210ade476c91f59c8c89309014455285
Rupak Vignesh Swaminathan <rupakvignesh@gmail.com>
2018-01-21 19:27:26 -0500

    global statistics included

--- scripts/compute_results_bss.m
+++ scripts/compute_results_bss.m
@@ -7,25 +7,45 @@ win_size = 640;
 hop_size = 320;
 sr = 16000;
 
-mask_path = '/Users/RupakVignesh/Desktop/fall17/7100/Singing-Voice-Separation/experiments/expt7/test_out/';
-test_feat_path = '/Users/RupakVignesh/Desktop/fall17/7100/MIR-1K/Test_feats/';
+mask_path = '/Users/RupakVignesh/Desktop/fall17/7100/Singing-Voice-Separation/experiments/expt6/test_out/';
 test_audio_path = '/Users/RupakVignesh/Desktop/fall17/7100/MIR-1K/Test/';
 
 output_files = dir(strcat(mask_path, '/*.csv'));
-test_feat_files = dir(strcat(test_feat_path, '/*.csv'));
+test_audio_files = dir(strcat(test_audio_path, '/*.wav'));
 
 N = length(output_files);
+SDR = zeros(2, N);
+SIR = zeros(2, N);
+SAR = zeros(2, N);
+GNSDR = zeros(2,1);
+GSIR = zeros(2,1);
+GSAR = zeros(2,1);
+total_L = 0;
+
 for i=1:N
-    [~, filename, ~] = fileparts(output_files(i).name);
-    estimated_mask = (csvread(strcat(mask_path,filename,'.csv')));
-    input_poly_spectra = (csvread(strcat(test_feat_path, filename,'.csv')));
-    estimated_vocal_spectra = estimated_mask .* input_poly_spectra;
-    estimated_vocal_spectra = estimated_vocal_spectra(:,1:513)';
+    [~, filename, ~] = fileparts(test_audio_files(i).name);
+    disp(filename);
+    est_vocal_spectra = abs(csvread(strcat('vocal_',mask_path,filename,'.csv')));
+    est_inst_spectra = abs(csvread(strcat('background_',mask_path,filename,'.csv')));
     [orig_sig, fs] = audioread(strcat(test_audio_path, filename, '.wav'));
-    rec_vocals = reconstruct_waveform(estimated_vocal_spectra, mean(orig_sig,2), win_size, hop_size, fs);
-    [SDR, SIR, SAR] = bss_eval(orig_sig(:,2), rec_vocals);
+    [rec_vocals, rec_inst] = reconstruct_waveform(est_vocal_spectra, est_inst_spectra, mean(orig_sig,2), win_size, hop_size, fs);
+    L = size(rec_vocals,1);
     
+    estimated_sources = [rec_inst, rec_vocals]';
+    mix_sources = [mean(orig_sig,2), mean(orig_sig,2)]';
     
+    [SDR(:,i), SIR(:,i), SAR(:,i)] = bss_eval_sources(estimated_sources, orig_sig');
+    [SDR_MIXED, ~, ~] = bss_eval_sources(orig_sig', mix_sources);
     
+    NSDR = SDR(:,i) - SDR_MIXED;
+    GNSDR = GNSDR + L*NSDR;
+    GSIR = GSIR + L*SIR(:,i);
+    GSAR = GSAR + L*SAR(:,i);
+    
+    total_L = total_L + L;
 end
+
+GNSDR = GNSDR/total_L;
+GSAR = GSAR/total_L;
+GSIR = GSIR/total_L;
 
