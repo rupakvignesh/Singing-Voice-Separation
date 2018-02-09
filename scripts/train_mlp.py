92666a04c6baefad35b9f2f33eee693e10ac8114
Rupak Vignesh Swaminathan <rupakvignesh@gmail.com>
2018-02-09 12:54:56 -0500

    validation with vocal gnsdr

--- scripts/train_mlp.py
+++ scripts/train_mlp.py
@@ -1,11 +1,11 @@
 """
-Implements a feed forward multilayer perceptron (3 hidden layers
-with 1024 hidden units each). Input is polyphonic musical mix and outputs
-are vocals and background accompaniment.
-Author: Rupak Vignesh Swaminathan
+Implements a stacked LSTM based DAE.
+Input: vocal + background
+Output: vocals (or an ideal binary mask)
 
 """
 import tensorflow as tf
+import librosa
 import numpy as np
 import os, sys
 from tf_methods import *
@@ -20,17 +20,25 @@ n_hidden2 = 1024
 n_hidden3 = 1024
 n_classes = n_input*n_steps # reconstruct input without background accompaniment
 learning_rate = 0.01
-num_epoch = 130
+num_epoch = 1000
 batch_size = 512
 
-experiments_folder='/home/rvignesh/singing_voice_separation/experiments/expt11'
+#Audio  parameters
+src_sr = 16000
+tgt_sr = 16000
+win_size = 640
+hop_size = 320
+fft_size = 1024
+
+experiments_folder='/home/rvignesh/singing_voice_separation/experiments/expt16'
 summaries_dir=experiments_folder+'/summaries'
 
 # tf Graph input
 print("Building computational graph")
+#x = tf.placeholder("float", [None, n_steps, n_input])
 background = tf.placeholder("float", [None, n_input*(2*num_context+1)], name='background')
 clean_vocals = tf.placeholder("float", [None, n_input*(2*num_context+1)], name='clean_vocals')
-x_mix = tf.placeholder("float", [None, n_input*(2*num_context+1)], name='mix') 
+x_mix = tf.placeholder("float", [None, n_input*(2*num_context+1)], name='mix') # tf.placeholder("float", [None, n_input*(2*num_context+1)]) 
 
 #first layer
 weights1 = tf.Variable(tf.random_normal([n_input*n_steps, n_hidden]), tf.float32, name='weights1')
@@ -53,39 +61,48 @@ biases4 = tf.Variable(tf.zeros([n_classes]), tf.float32, name='biases4')
 weights5 = tf.Variable(tf.random_normal([n_hidden3, n_classes]), tf.float32, name='weights5')
 biases5 = tf.Variable(tf.zeros([n_classes]), tf.float32, name='biases5')
 
-voc_tf_pred = tf.nn.relu(tf.add(tf.matmul(hidden3_output,weights4), biases4))		#Time freq mask predictions for vocals
-back_tf_pred = tf.nn.relu(tf.add(tf.matmul(hidden3_output,weights5), biases5))		#Time freq mask predictions for background
+voc_tf_pred = tf.nn.relu(tf.add(tf.matmul(hidden3_output,weights4), biases4))
+back_tf_pred = tf.nn.relu(tf.add(tf.matmul(hidden3_output,weights5), biases5))
 
-est_vocals = (voc_tf_pred/(voc_tf_pred + back_tf_pred + 0.001)) * x_mix			#Deterministic layer for computing magnitude spectra of vocals
-est_background = (back_tf_pred/(voc_tf_pred + back_tf_pred + 0.001)) * x_mix		#Deterministic layer for computing magnitude spectra of background
+est_vocals = (voc_tf_pred/(voc_tf_pred + back_tf_pred + 0.001)) * x_mix 
+est_background = (back_tf_pred/(voc_tf_pred + back_tf_pred + 0.001)) * x_mix
 
 est_vocals = tf.identity(est_vocals, name='estimated_vocals')
 est_background = tf.identity(est_background, name='estimated_background')
 
+# Loss function
 with tf.name_scope("Loss"):
     loss = tf.reduce_mean(tf.square(est_vocals - clean_vocals) + tf.square(est_background - background))
     #print(current_frame_gain.get_shape(), "Current Frame Gain Shape")
 
-
 loss1 = tf.summary.scalar("loss1", loss)
+#Train op
 train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 print("Reading Data")
 train_feats, _ = read_data(sys.argv[1])         # Clean Vocals
 noise_feats_train, _ = read_data(sys.argv[2])   # Background track
-valid_feats, _ = read_data(sys.argv[3])
-noise_feats_valid, _ = read_data(sys.argv[4])
+valid_back, valid_voc, valid_mixed = get_wav_from_path(sys.argv[3], src_sr, tgt_sr)
+valid_back_stft, valid_voc_stft, valid_mix_stft = list(map(lambda x: get_stft_from_wav(x, fft_size, hop_size, win_size), [valid_back, valid_voc, valid_mixed]))
+
+#print("Normalizing")
+#train_mean = np.mean(train_feats,axis=0)
+#train_std = np.std(train_feats,axis=0)
+#train_feats = (train_feats - train_mean)/(train_std)
+#back_mean = np.mean(noise_feats_train,axis=0)
+#back_std = np.std(noise_feats_train,axis=0)
+#noise_feats_train = (noise_feats_train - back_mean)/(back_std)
+#print(np.shape(train_feats), "Train feats shape")
 
 print("Add context")
 train_feats_context = splice_feats(train_feats, num_context)
 noise_feats_train_context = splice_feats(noise_feats_train, num_context)
-valid_feats_context = splice_feats(valid_feats, num_context)
-noise_feats_valid_context = splice_feats(noise_feats_valid, num_context)
+valid_feats_mix_context = [splice_feats(np.abs(valid_feats.T), num_context) for valid_feats in valid_mix_stft]
 print(np.shape(train_feats_context), "Train feats shape after slice feats")
 
 print ("Training Neural Network")
 num_train_instance, _= np.shape(train_feats_context)
-saver = tf.train.Saver(max_to_keep=10)
+saver = tf.train.Saver(max_to_keep=15)
 with tf.Session() as sess:
     merged = tf.summary.merge_all()
     train_writer = tf.summary.FileWriter(summaries_dir+'/train', sess.graph)
@@ -95,7 +112,7 @@ with tf.Session() as sess:
     for i in range(num_epoch):
         sess_loss = 0.0
 
-	shuffle = np.random.permutation(len(noise_feats_train_context))			#Randomize background to virtually create more data (augmentation technique)
+        shuffle = np.random.permutation(len(noise_feats_train_context))			#Shuffling the background to virtually create more data
         for batch_ind in range(0,num_train_instance,batch_size):
             batch_noise = noise_feats_train_context[shuffle[batch_ind:batch_ind+batch_size]]
             batch_vocals = train_feats_context[batch_ind:batch_ind+batch_size]
@@ -104,10 +121,25 @@ with tf.Session() as sess:
             _, batch_loss, train_summary = sess.run([train, loss, merged], feed_dict={clean_vocals: batch_vocals, background: batch_noise, x_mix: batch_mix})
             sess_loss += batch_loss
         train_writer.add_summary(train_summary, i)
-        valid_summary, valid_loss = sess.run([merged, loss], feed_dict={clean_vocals: valid_feats_context, background: noise_feats_valid_context, x_mix: valid_feats_context+noise_feats_valid_context})
-        valid_writer.add_summary(valid_summary,i)
+        
+        pred_vocal_mag = []
+        pred_back_mag = []
+        for mix in valid_feats_mix_context:
+                [a1, a2] = sess.run([est_vocals, est_background], feed_dict={x_mix: mix})
+                pred_vocal_mag.append(np.split(a1, n_steps, axis=1)[num_context])
+                pred_back_mag.append(np.split(a2, n_steps, axis=1)[num_context])
+        pred_phase = [np.angle(mix) for mix in valid_mix_stft]
+        pred_vocal = get_wav_from_stft(pred_vocal_mag, pred_phase, hop_size, win_size)
+        pred_back = get_wav_from_stft(pred_back_mag, pred_phase, hop_size, win_size)
+        GNSDR, GSIR, GSAR = global_bss_metrics(valid_back, valid_voc, valid_mixed, pred_back, pred_vocal)
+        #valid_writer.add_summary(sess.run(tf.summary.scalar('GNSDR_vocal', tf.constant(GNSDR[1]))), i)
+        #valid_writer.add_summary(sess.run(tf.summary.scalar('GSIR_vocal', tf.constant(GSIR[1]))), i)
+        #valid_writer.add_summary(sess.run(tf.summary.scalar('GSAR_vocal', tf.constant(GSAR[1]))), i)
+        #valid_writer.add_summary(sess.run(tf.summary.audio('Pred_vocal', tf.constant(pred_vocal[0]), tgt_sr)), i) 
+        
+        if ((i+1)%10==0):
+            saver.save(sess, experiments_folder+'/saved_models/model', global_step=i+1)
 
-        saver.save(sess, experiments_folder+'/saved_models/model', global_step=i+1)
-
-        print("Epoch "+str(i)+ " Train loss", sess_loss/(num_train_instance/batch_size), "Valid loss", valid_loss)
+        print("Epoch "+str(i)+ " Train loss", sess_loss/(num_train_instance/batch_size), "GNSDR vocal validation ",str(GNSDR[1]))
+    
 
