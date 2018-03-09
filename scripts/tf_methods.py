03aad3375dac23412d40193fda1df174095d205c
Rupak Vignesh Swaminathan <rupakvignesh@gmail.com>
2018-03-09 12:50:03 -0500

    libraso helper methods and bss evaluation added

--- scripts/tf_methods.py
+++ scripts/tf_methods.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import os
+import librosa
+import mir_eval
+import re 
+import pdb
 
 def read_data(filename):
     """
@@ -43,6 +48,28 @@ def splice_feats(features, NUM_CONTEXT):
             features_with_context[i] = extended_features[i:i+(2*NUM_CONTEXT+1)].reshape(1,(2*NUM_CONTEXT+1)*m)
 
         return features_with_context
+
+def splice_feats_with_round(features, NUM_CONTEXT):
+    """
+    Adds (left and right) context to the input features.
+    The number of frames to include is
+    defined by NUM_CONTEXT.
+    Returns an nparray of features with context
+    """
+    if NUM_CONTEXT==0:
+        return features
+    else:
+        extended_features = np.array([])
+        n,m = np.shape(features)
+        features_with_context = np.zeros((n,m*(2*NUM_CONTEXT+1)))
+        temp = np.zeros((NUM_CONTEXT*m)).reshape(NUM_CONTEXT,m)
+        extended_features = np.concatenate((temp,features,temp))
+
+        for i in range(n):
+            features_with_context[i] = extended_features[i:i+(2*NUM_CONTEXT+1)].reshape(1,(2*NUM_CONTEXT+1)*m)
+
+        return features_with_context
+
 
 def get_classification_accuracy(network_outputs, true_labels):
     """
@@ -104,19 +131,18 @@ def auto_encoder(dimensions,tied_weights=True, NUM_CONTEXT=0, optimizer = tf.tra
     input tensor, weights and biases of the layers, loss
     """
 
-    x = tf.placeholder(tf.float32, [None, dimensions[0]], name='x')
-    n = tf.placeholder(tf.float32, [None, dimensions[0]], name='n')
-    current_input = x+n
-    x_corrupt = current_input
+    x = tf.placeholder(tf.float32, [None, dimensions[0]], name='signal')
+    n = tf.placeholder(tf.float32, [None, dimensions[0]], name='noise')
+    current_input = tf.add(x,n, name='corrupt_signal')
 
     #Encoder
     encoder=[]
     for layer_i, n_output in enumerate(dimensions[1:]):
         n_input = int(current_input.get_shape()[1])
-        W = tf.Variable(tf.random_normal([n_input, n_output]), tf.float32)  #Weights
-        b = tf.Variable(tf.zeros([n_output]), tf.float32)                   #Biases
+        W = tf.Variable(tf.random_normal([n_input, n_output]), tf.float32, name="W"+str(layer_i+1))  #Weights
+        b = tf.Variable(tf.zeros([n_output]), tf.float32, name="b"+str(layer_i+1))                   #Biases
         encoder.append([W,b])
-        output = tf.nn.sigmoid(tf.matmul(current_input, W) +b)
+        output = tf.nn.sigmoid(tf.add(tf.matmul(current_input, W),b), name="outputE"+str(layer_i+1))
         current_input = output
 
     # latent representation
@@ -126,37 +152,30 @@ def auto_encoder(dimensions,tied_weights=True, NUM_CONTEXT=0, optimizer = tf.tra
     #Decoder
     decoder = []
     for layer_i, n_output in enumerate(dimensions[:-1][::-1]):
-        W = tf.Variable(tf.random_normal((encoder[layer_i][0]).get_shape().as_list()), tf.float32)
-        b = tf.Variable(tf.zeros(n_output), tf.float32)
+        b = tf.Variable(tf.zeros(n_output), tf.float32, name="bd"+str(layer_i+1))
         if (tied_weights):
             W = tf.transpose(encoder[layer_i][0])
+        else:
+            W = tf.Variable(tf.random_normal(tf.transpose(encoder[layer_i][0]).get_shape().as_list()), tf.float32, name="Wd"+str(layer_i+1))
         decoder.append([W,b])
-        output = tf.nn.sigmoid(tf.matmul(current_input, W) + b)
+        output = tf.nn.sigmoid(tf.add(tf.matmul(current_input, W), b), name="outputD"+str(layer_i+1))
         current_input = output
 
     # Reconstructed output
     y = current_input
     encoder.reverse()
 
-    I = tf.placeholder(tf.float32,[None, dimensions[0]*(2*NUM_CONTEXT+1)], name='I')
-    dec_W = tf.placeholder(tf.float32)
-    if pretrain:
-        O = tf.nn.sigmoid(tf.matmul(y, dec_W))
-
     #loss function
     with tf.name_scope("loss"):
         split_inputs = tf.split(x, 2*NUM_CONTEXT+1, 1)
         split_outputs = tf.split(y, 2*NUM_CONTEXT+1, 1)
-        if pretrain:
-            split_inputs = tf.split(I, 2*NUM_CONTEXT+1, 1)
-            split_outputs = tf.split(O, 2*NUM_CONTEXT+1, 1)
         center_frame_x = split_inputs[NUM_CONTEXT]
         center_frame_y = split_outputs[NUM_CONTEXT]
         loss = tf.reduce_mean(tf.square(center_frame_y-center_frame_x)) # Reduce mean squared error
 
     train = optimizer.minimize(loss)
 
-    return ({'x': x, 'n':n, 'I': I, 'dec_W': dec_W, 'encoder':encoder, 'decoder': decoder, "y": y, "loss":loss, "train":train, "z":z})
+    return ({'x': x, 'n':n, 'encoder':encoder, 'decoder': decoder, "y": y, "loss":loss, "train":train, "z":z})
 
 
 def auto_encoder_gain_mask(dimensions,tied_weights=True, NUM_CONTEXT=0, optimizer = tf.train.AdamOptimizer(0.01), pretrain=False, lamb=0, binarize_gain=False):
@@ -173,10 +192,8 @@ def auto_encoder_gain_mask(dimensions,tied_weights=True, NUM_CONTEXT=0, optimize
     """
 
     x = tf.placeholder(tf.float32, [None, dimensions[0]], name='x')
-    stddev = np.random.uniform(0,1.5)
-    noise = tf.random_normal(shape=tf.shape(x), mean=0.0, stddev=stddev, dtype=tf.float32)
-    x_corrupt = x+noise
-    current_input = x_corrupt
+    n = tf.placeholder(tf.float32, [None, dimensions[0]], name='n')
+    current_input = x+n
     #Encoder
     encoder=[]
     for layer_i, n_output in enumerate(dimensions[1:]):
@@ -206,9 +223,9 @@ def auto_encoder_gain_mask(dimensions,tied_weights=True, NUM_CONTEXT=0, optimize
     y = current_input
     encoder.reverse()
 
-    gain = tf.abs(x)/(tf.abs(x)+tf.abs(noise))  #Oracle gain
+    gain = tf.abs(x)/(0.00001+tf.abs(x)+tf.abs(n))  #Oracle gain
     if binarize_gain:
-        gain_thres = 0.7
+        gain_thres = 0.9
         gain = (gain>gain_thres)
         gain = tf.cast(gain,tf.float32)
 
@@ -235,7 +252,7 @@ def auto_encoder_gain_mask(dimensions,tied_weights=True, NUM_CONTEXT=0, optimize
 
     train = optimizer.minimize(loss)
 
-    return ({'x': x,'gain': center_frame_gain, 'encoder':encoder, 'decoder': decoder, "y": y, "loss":loss, "train":train, "z":z})
+    return ({'x': x, 'n':n, 'gain': center_frame_gain, 'encoder':encoder, 'decoder': decoder, "y": y, "loss":loss, "train":train, "z":z})
 
 def fine_tune_layers(input_dim, encoder_val, decoder_val, tied_weights=True, NUM_CONTEXT=0, act=tf.nn.sigmoid, optimizer = tf.train.AdamOptimizer(0.01)):
     """
@@ -329,3 +346,115 @@ def restore_graph(experiment_folder, model, key):
         bd1 = tf.get_collection(key+'_D')[1]    #Decoder weights
         [W1,b1, Wd1, bd1] = sess.run([W1,b1, Wd1, bd1])
         return [W1,b1,Wd1,bd1]
+
+def natural_sort_key(s):
+    _nsre = re.compile('([0-9]+)')
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+def get_wav_from_path(wav_path, src_sr, tgt_sr):
+    """
+    Returns a list of numpy nd-arrays of wav files 
+    present in the wav_path (absolute path)
+    wav_path - absolute path where wavefiles are present
+    src_sr - source sampling rate in Hz
+    tgt_sr - target sampling rate in Hz
+    """
+    wav_files = []
+    items = os.listdir(wav_path)
+    items.sort(key=natural_sort_key)
+    for name in items:
+        if name.endswith(".wav"):
+            wav_files.append(os.path.join(wav_path,name))
+
+    read_batch = lambda file_path: librosa.resample(librosa.load(file_path, sr=src_sr, mono=False)[0], src_sr, tgt_sr)
+    wav_audio = list(map(read_batch, wav_files))	#List of numpy arrays containing audio files
+    background_wav = [wav[0,:]/np.sqrt(np.sum(wav[0,:]**2)) for wav in wav_audio]
+    vocal_wav = [wav[1,:]/np.sqrt(np.sum(wav[1,:]**2)) for wav in wav_audio]
+    mixed_wav = [background_wav[i] + vocal_wav[i] for i in range(len(wav_audio))]	#Mix at 0dB
+    return background_wav, vocal_wav, mixed_wav
+
+def get_stft_from_wav(wav_audio, fft_size, hop_size, win_size):
+    """
+    Get Frequency domain representation of 
+    the waveforms. Returns a list of complex stft
+    wav_audio - list of wavfiles (as returned by get_wav_from_path function)
+    fft_size - N-point of the FFT in samples
+    hop_size - FFT hop in samples
+    win_size - analysis window in samples
+    """
+    compute_stft = lambda s: librosa.stft(s, fft_size, hop_size, win_size)
+    complex_stft = list(map(compute_stft, [np.array(wav) for wav in wav_audio]))
+    return complex_stft
+
+def get_gt_from_path(gt_path):
+    """
+    Get groundtruth(0/1) corresponding to the audio frames.
+    """
+    gt_files = []
+    items = os.listdir(gt_path)
+    items.sort(key=natural_sort_key)
+    for name in items:
+        if name.endswith(".vocal"):
+            gt_files.append(os.path.join(gt_path,name))
+    gt = []
+    for i in range(len(gt_files)):
+        with open(gt_files[i]) as F:
+            gt_lines = list(map(int, [lines.rstrip() for lines in F]))
+        F.close()
+        gt.append(np.array(gt_lines)[None,:])
+    return gt
+
+def get_mel_spec_from_wav(wav_audio, sr, fft_size, hop_size, n_mels):
+    """
+    Get Mel Spectrograms the waveforms. 
+    Returns a list of mel spectrograms
+    wav_audio - list of wavfiles (as returned by get_wav_from_path function)
+    sr - sampling rate in Hz
+    fft_size - N-point of the FFT in samples
+    hop_size - FFT hop in samples
+    n_mels = number of mel bands
+    """ 
+    compute_mel_spec = lambda s: librosa.feature.melspectrogram(s, sr=sr, n_fft=fft_size, hop_length=hop_size, n_mels=n_mels) 
+    mel_spec = list(map(compute_mel_spec, [np.array(wav) for wav in wav_audio]))
+    return mel_spec
+
+def get_wav_from_stft(mag_spec, phase_spec, hop_size, win_size):
+    """
+    Reconstruction of waveforms from stft
+    mag_spec - list of Magnitude spectra
+    phase_spec - list of phase spectra
+    hop_size - FFT hop in samples
+    win_size - analysis window in samples
+    """
+    compute_istft = lambda s: librosa.istft(s[0]*np.exp(1.j*s[1]), hop_size, win_size)	#m*np.exp(1.j*p) makes it complex stft
+    wav_audio = list(map(compute_istft, [(np.array(mag_spec[i].T), np.array(phase_spec[i])) for i in range(len(mag_spec))]))
+    return wav_audio	#List of numpy arrays containing audio files
+
+def global_bss_metrics(background_wav, vocal_wav, mixed_wav, pred_background_wav, pred_vocal_wav):
+    """
+    Returns global NSDR, SIR and SAR
+    for vocals and background
+    back_wav - list of numpy nd array with background wavfiles
+    vocal_wav
+    # bss_eval_sources(np.array([src1_wav[i], src2_wav[i]]),
+                                              np.array([mixed_wav[i], mixed_wav[i]]), False)
+    """
+    GNSDR = np.zeros(2)
+    GSIR = np.zeros(2)
+    GSAR = np.zeros(2)
+    total_len = 0.0
+    for i in range(len(vocal_wav)):
+        wav_len = min(len(vocal_wav[i]), len(pred_vocal_wav[i])) 
+        sdr, sir, sar, _ = mir_eval.separation.bss_eval_sources(np.array([background_wav[i][:wav_len], vocal_wav[i][:wav_len]]), np.array([pred_background_wav[i][:wav_len], pred_vocal_wav[i][:wav_len]]), False)
+        sdr_m, _, _, _ = mir_eval.separation.bss_eval_sources(np.array([mixed_wav[i][:wav_len], mixed_wav[i][:wav_len]]), np.array([background_wav[i][:wav_len], vocal_wav[i][:wav_len]]), False)
+        nsdr = sdr - sdr_m
+        GNSDR += nsdr*wav_len
+        GSIR += sir*wav_len
+        GSAR += sar*wav_len
+        total_len += wav_len
+    GNSDR = GNSDR/total_len
+    GSIR = GSIR/total_len
+    GSAR = GSAR/total_len
+    return GNSDR, GSIR, GSAR
+    
